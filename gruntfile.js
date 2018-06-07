b06ded1f45f96a27d3ed0a1764ece5f70899f9e4
juanitolaguna <little.pilgrim@mail.ru>
2018-06-07 17:13:39 +0200

    add gruntfile - grunticon config

---
+++ gruntfile.js
@@ -0,0 +1,33 @@
+module.exports = function(grunt) {
+	grunt.initConfig({
+		pkg: grunt.file.readJSON('package.json'),
+		grunticon: {
+			myIcons: {
+				files: [{
+					expand: true,
+					cwd: 'images/icons/',
+					src: ['*.svg'],
+					dest: 'example/output/svg'
+				}],
+				options: {
+					enhanceSVG: true,
+					pngpath: '/example/png',
+				}
+			}
+		}
+
+	});
+
+	grunt.loadNpmTasks('grunt-grunticon');
+	grunt.registerTask('make-icons', ['grunticon:myIcons']);
+}
+
+
+
+
+
+
+
+
+
+
